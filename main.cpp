eea30e5c3acebe624a6763317f0ded9ddbf85dc6
mvrublebsk <mvrublevsk@student.ucode.world>
2021-12-30 15:28:41 +0200

    final vrs

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstring>
+#include <stdlib.h>
+
+#define BUFFSIZE 5
+
+void myout(char *str);
+
+int main() {
+    char str[] = "Hello asd  G\n";
+
+    myout(str);
+
+    return 0;
+}
+
+void myout(char *str) {
+    bool flag = 0;
+    int_fast8_t count = 0;
+    int_fast16_t i = 0;
+    unsigned char *buff = new unsigned char[BUFFSIZE];
+
+    for ( ; i <= strlen(str); ) {
+        if (count == BUFFSIZE || (flag = i == strlen(str))) {
+            printf("%s", buff);
+
+            count = 0;
+
+            if (flag)
+                break;
+
+        } else {
+            buff[count] = str[i];
+            ++count;
+            ++i;
+        }
+
+        buff[count + 1] = '\0';
+    }
+
+    delete[] buff;
+}
